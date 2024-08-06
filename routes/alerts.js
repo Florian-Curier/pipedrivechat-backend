d970099f2abf0ae11146fcc28040dc42a2a488a7
thegreatinternetlord <msg.johan.navarro@gmail.com>
2024-08-06 16:22:52 +0200

    route GET alert

--- routes/alerts.js
+++ routes/alerts.js
@@ -4,8 +4,9 @@ var router = express.Router();
 // Ecrire les routes ici
 
 const Alert = require("../models/alerts")
+const User = require("../models/users")
 
-router.post("/alerts", (req,res) => {
+router.post("/", (req,res) => {
     
     Alert.find({}).then(data => {
 
@@ -16,4 +17,36 @@ router.post("/alerts", (req,res) => {
     })
     })
 })
+
+router.get('/test', (req, res) => {
+    Alert.find()
+    .populate('user_id')
+    .then(data => {
+        
+      res.json({ allUsers: data });
+    });
+   });
+
+
+
+ router.get('/:company_id/:user_id', (req, res) => {
+  User.findOne({ pipedrive_user_id: req.params.user_id, pipedrive_company_id: req.params.company_id })
+  .then (userData => {
+        console.log(userData)
+
+        Alert.find({user_id : userData._id})
+        .populate('trigger_id')
+        
+        .then(alertData => {
+            console.log(alertData)
+            res.json({ alert: alertData });
+
+        });
+  });
+
+}); 
+
+
+
+
 module.exports = router;
