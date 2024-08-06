832cfdefb61fd93a825cd92bdd45d4c182c7bda1
Felix <felix.dennery@gmail.com>
2024-08-06 16:07:04 +0200

    ajout channel name schema alerts

--- models/alerts.js
+++ models/alerts.js
@@ -5,6 +5,7 @@ const alertSchema = mongoose.Schema({
     alert_name: String,
 	pipedrive_webhook_id: String,
     google_channel_id: String,
+    google_channel_name: String,
     user_id : {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
     trigger_id : {type: mongoose.Schema.Types.ObjectId, ref: 'triggers'},
     creation_date : {type:Date,default:Date.now()},
