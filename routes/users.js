5ba2a430f29c7a38b63984050b3da18558e6f95c
Felix <felix.dennery@gmail.com>
2024-08-14 14:34:51 +0200

    route logout

--- routes/users.js
+++ routes/users.js
@@ -33,65 +33,52 @@ router.get('/channels/:company_id/:user_id', async (req, res) => {
 
   try {
 
-  let userData = await User.findOne({ pipedrive_user_id: req.params.user_id, pipedrive_company_id: req.params.company_id })
-
-  if (userData === null) { 
+    let userData = await User.findOne({ pipedrive_user_id: req.params.user_id, pipedrive_company_id: req.params.company_id })
+    if (userData === null) {
       return res.status(404).json({ result: false, error: 'User Not Found' })
-  }
+    }
 
- // Après avoir trouvé un user, vérification de la validité du token pour le rafraichir si besoin
-
+    // Après avoir trouvé un user, vérification de la validité du token pour le rafraichir si besoin
     const expirationDate = new Date(userData.google_tokens.expiration_date).getTime()
     if (Date.now() > expirationDate) {
-
-        const tokens = await refreshGoogleToken(userData)
-
-        if (!tokens.result) {
+      const tokens = await refreshGoogleToken(userData)
+      if (!tokens.result) {
         // Si le refresh token ne fonctionne pas on renvoie la réponse de Google et un statut 401
-          console.log('failed refresh token')
-         return res.status(401).json(tokens)
- 
-        } else {
-          console.log('token refreshed')
-        }
-       
-        
-         // Puis on met à jour la variable user avec les nouvelles donéées 
-          userData = await User.findOne({_id: userData._id }) 
-    
-        } 
+        console.log('failed refresh token')
+        return res.status(401).json(tokens)
 
-        // Fetch de l'api Google pour récupérer les channels , une fois qu'on est sur d'avoir un token valide
-        const channelsResponse = await fetch('https://chat.googleapis.com/v1/spaces', {
-          headers: { 'Authorization': `Bearer ${userData.google_tokens.access_token}` }
-        })
-    
-        const channels = await channelsResponse.json()
-    
-    
-        // Si réponse ok on renvoie la liste des channels avec un filtre pour exclure les DM
-    
-        if (channelsResponse.ok) {
-    
-            let channelsFiltered = channels.spaces.filter(e => e.spaceType ==='SPACE')
-            res.json({ result: true, channels: channelsFiltered })
-
-        } else {
-    
-        // Si erreur on renvoie le code dans le HTTP et le message d'erreur dans error
-          
-          res.status(channels.error.code).json({ result: false, error: channels.error })
-        }
-
+      } else {
+        console.log('token refreshed')
       }
-      catch (err) {
-        // Catch des erreurs potentielles
-        
-        console.log(err)
-        res.status(500).json({ result: false, error: 'Server Error' })
-      }
+      // Puis on met à jour la variable user avec les nouvelles donéées 
+      userData = await User.findOne({ _id: userData._id })
 
-    });
+    }
+
+    // Fetch de l'api Google pour récupérer les channels , une fois qu'on est sur d'avoir un token valide
+    const channelsResponse = await fetch('https://chat.googleapis.com/v1/spaces', {
+      headers: { 'Authorization': `Bearer ${userData.google_tokens.access_token}` }
+    })
+
+    const channels = await channelsResponse.json()
+    // Si réponse ok on renvoie la liste des channels avec un filtre pour exclure les DM
+
+    if (channelsResponse.ok) {
+      let channelsFiltered = channels.spaces.filter(e => e.spaceType === 'SPACE')
+      res.json({ result: true, channels: channelsFiltered })
+
+    } else {
+      // Si erreur on renvoie le code dans le HTTP et le message d'erreur dans error   
+      res.status(channels.error.code).json({ result: false, error: channels.error })
+    }
+  }
+  catch (err) {
+    // Catch des erreurs potentielles
+    console.log(err)
+    res.status(500).json({ result: false, error: 'Server Error' })
+  }
+
+});
 
 
 // ROUTE GET WEBHOKKS BY USER : remonte tous les webhooks d'un user
@@ -100,46 +87,57 @@ router.get('/webhooks/:company_id/:user_id', async (req, res) => {
 
   try {
 
-  let userData = await User.findOne({ pipedrive_user_id: req.params.user_id, pipedrive_company_id: req.params.company_id })
-
-  if (userData === null) { 
+    let userData = await User.findOne({ pipedrive_user_id: req.params.user_id, pipedrive_company_id: req.params.company_id })
+    if (userData === null) {
       return res.status(404).json({ result: false, error: 'User Not Found' })
+    }
+
+    // Après avoir trouvé un user, vérification de la validité du token pour le rafraichir si besoin
+    const expirationDate = new Date(userData.pipedrive_tokens.expiration_date).getTime()
+    if (Date.now() > expirationDate) {
+      const tokens = await refreshPipedriveToken(userData)
+      if (!tokens.result) {
+        // Si le refresh token ne fonctionne pas on renvoie la réponse de Google et un statut 401
+        return res.status(401).json(tokens)
+      }
+    }
+
+    // Mise à jour du sur puis fetch des webhooks
+    userData = await User.findOne({ _id: userData._id })
+    const webhooksResponse = await fetch(`${userData.api_domain}/v1/webhooks`, {
+      headers: { 'Authorization': `Bearer ${userData.pipedrive_tokens.access_token}` }
+    })
+    const webhooksData = await webhooksResponse.json()
+    res.json({ result: true, webhooks: webhooksData.data })
+
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ result: false, error: 'Server Error' })
+  }
+});
+
+// ROUTE DE LOGOUT GOOGLE
+
+router.put('/googleLogout/', async (req, res) => {
+
+  const { pipedrive_user_id, pipedrive_company_id } = req.body
+
+  const updateUser = await User.updateOne({pipedrive_user_id, pipedrive_company_id },
+                          {google_tokens: null, google_email: null})
+  
+  if (updateUser.modifiedCount > 0) {
+
+    res.json({result: true, message: 'User Logged out' })
+  } else {
+    res.status(500).json({result: false, error: 'Unable to logout user'})
   }
 
- // Après avoir trouvé un user, vérification de la validité du token pour le rafraichir si besoin
-
-    const expirationDate = new Date(userData.pipedrive_tokens.expiration_date).getTime()
-    
-    if (Date.now() > expirationDate) {
-
-        const tokens = await refreshPipedriveToken(userData)
-
-        if (!tokens.result) {
-        // Si le refresh token ne fonctionne pas on renvoie la réponse de Google et un statut 401
-         return res.status(401).json(tokens)
- 
-        } 
-      }
-
-      // Mise à jour du sur puis fetch des webhooks
-
-       userData = await User.findOne({_id : userData._id})
-
-      const webhooksResponse = await fetch(`${userData.api_domain}/v1/webhooks`, {
-        headers: {'Authorization' : `Bearer ${userData.pipedrive_tokens.access_token}`}
-      })
-      const webhooksData = await webhooksResponse.json()
-
-      res.json({result : true, webhooks : webhooksData.data})
-
-      }catch(err) {
-
-        console.log(err)
-
-      res.status(500).json({result : false, error : 'Server Error'})
-      }});
 
 
+
+
+
+})
 
 
 module.exports = router;
