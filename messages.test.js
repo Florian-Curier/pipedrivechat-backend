68a25451033465c79358470a25e8efd757d96a89
mickael <robert.mickael28@gmail.com>
2024-09-08 18:05:23 +0200

    Update du test de la route message/channel

--- messages.test.js
+++ messages.test.js
@@ -2,19 +2,18 @@ const request = require('supertest');
 const app = require('./app');
 
 
-it('GET messages/all/:pipedrive_company_id/:pipedrive_user_id/:startDate/:endDate/:timeUnit', async () => {
-	const res = await request(app).get('/messages/all/13476443/21658061')
-
-	expect(res.statusCode).toBe(200)
-	expect(res.body.result).toEqual(true)
-})
-
-
 it('GET messages/channel/:google_channel_id/:startDate/:endDate/:timeUnit', async () => {
-	const res = await request(app).get('/messages/channel/AAAAbAp0eik')
+	const res = await request(app).get('messages/channel/AAAAbAp0eik/null/null/year')
 
 	expect(res.statusCode).toBe(200)
 	expect(res.body.result).toEqual(true)
+	expect(res.body.result).toEqual([
+		{
+		  "time": "2024",
+		  "value": 1
+		}
+	  ]
+	)
 })
 
 it('POST messages', async () => {
